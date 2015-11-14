5d019cbda363e53e330afeb0b36c4410e3ebe7bc
Abhinav Khanna <akhanna@princeton.edu>
2015-11-14 01:09:04 -0500

    added a basic api beginning

--- api.py
+++ api.py
@@ -1,1 +1,16 @@
 __author__ = 'abkhanna'
+import web
+
+urls = (
+    '/sticky/board', 'BoardDetail',
+)
+
+class BoardDetail:
+    def GET(self):
+        # shows the board page
+        render = web.template.render('templates')
+        return render.board()
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
